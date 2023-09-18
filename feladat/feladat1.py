456d0f5dd6dc6f15c6e614c0f276a9bf60374eb5
Rózsás Viktor Szilárd <rozsasv22@ADY-NAGYATAD.SULINET.HU>
2023-09-18 08:45:04 +0200

    as

---
+++ feladat/feladat1.py
@@ -0,0 +1,9 @@
+b= 12
+a= 13
+
+if a==b:
+    print("a két szám egyenlő")
+elif a<b:
+    print("a második szám a nagyobb nagyobb")
+else :
+    print(" az első szám a nagyobb")    
---
+++ feladat/feladat3.py
@@ -0,0 +1,12 @@
+szam= 200
+
+szam+=10
+
+if szam<50:
+    szam/=2
+    print(szam)
+else:
+    szam*=10
+    print(szam)
+
+print 10.5%1
