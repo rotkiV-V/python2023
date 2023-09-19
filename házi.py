697ae71b28cd1dde4b5e39fbf5dbe605be2afe04
Rózsás Viktor Szilárd <rozsasv22@ADY-NAGYATAD.SULINET.HU>
2023-09-19 11:49:34 +0200

    ahgsd

---
+++ házi.py
@@ -0,0 +1,13 @@
+szam=535
+
+if szam<10:
+    print("a szám 1 jegyű")
+
+elif szam<100:
+    print("a szám 2 jegyű")
+
+elif szam<1000:
+    print("a szám 3 jegyű")
+
+else:
+    print("a szám 4 jegyű")
